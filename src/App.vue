5a893330dcbebce991c863f9ba1bb9c56832a0a0
aimeariadne <aime@multiplier.com.br>
2023-05-26 11:28:48 +0100

    initialize project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { RouterView } from 'vue-router'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    RouterView
+  }
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
+
