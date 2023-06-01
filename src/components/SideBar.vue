de389473d21dd2f69740cb98cd3f08dd5ef61e1f
aimeariadne <aime@multiplier.com.br>
2023-06-01 11:55:48 +0100

    create navbar and project route

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,8 +1,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { RouterLink } from 'vue-router'
 
 export default defineComponent({
   name: 'SideBar',
+  components: {
+    RouterLink,
+  },
   emits: [ 'DarkModeToggle' ],
   data() {
     return {
@@ -37,6 +41,28 @@ export default defineComponent({
     >
       <i :class="darkModeIcon"></i>
     </button>
+    <nav class="mt-5">
+      <ul>
+        <li>
+          <RouterLink 
+            class="link"
+            to="/"
+          >
+            <i class="fas fa-tasks"></i>
+            Tasks
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink 
+            class="link"
+            to="/projects"
+          >
+            <i class="fas fa-project-diagram"></i>
+            Projects
+          </RouterLink>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -53,5 +79,17 @@ header {
     padding: 2.5rem;
     height: auto;
   }
+}
+nav li {
+  margin: 0.5rem 0;
+}
+.link {
+  color: #485fc7;
+}
+.link.router-link-active {
+    color: #2b2d42 ;
+}
+.link:hover {
+  color: #fcc34f;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import TaskView from '@/views/TaskView.vue'
+import ProjectsView from '@/views/ProjectsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'tasks',
       component: TaskView
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: ProjectsView
     },
   ] as RouteRecordRaw[]
 })
