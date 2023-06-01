4e0f81e4bc898df0bc3162cb84381b16a1558ff6
aimeariadne <aime@multiplier.com.br>
2023-06-01 11:54:49 +0100

    add pinia to the project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createPinia } from 'pinia'
 
+import router from './router'
+import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(router)
-
-app.mount('#app')
+app.use(router).use(pinia).mount('#app')
